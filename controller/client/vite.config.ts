93127aae6e5cab642e03347ede2722d557ffc81f
Glitch752 <xxGlitch752xx@gmail.com>
2025-01-18 23:53:42 -0600

    Begin migrating client to use Svelte

--- controller/client/vite.config.ts
+++ controller/client/vite.config.ts
@@ -1,8 +1,10 @@
-import type { UserConfig } from 'vite'
+import type { UserConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   build: {
     outDir: '../static',
     emptyOutDir: true,
-  }
+  },
+  plugins: [svelte()],
 } satisfies UserConfig;
